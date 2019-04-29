892d753dbba6de8139d62e2a6e776cd9a0cc6d57
aschuliger <volleydog26@gmail.com>
2019-04-29 11:18:56 -0400

    Fixed the bubble chart

--- dataVisualizations/TestBubble/js/main.js
+++ dataVisualizations/TestBubble/js/main.js
@@ -17,38 +17,29 @@ Plotly.d3.csv("../../dataMining/avocado-with-regions.csv", function(err, rows){
     }
 
     var regions = ['GreatLakes', 'Midsouth', 'Northeast', 'NorthernNewEngland', 'Plains', 'SouthCentral', 'Southeast', 'West', 'WestTexNewMexico'];
-    var prices = new Array(10);
-    var bags = new Array(10);
+    var prices = new Array(9);
+    var bags = new Array(9);
     var total = 0;
 
-    for(var x = 0; x < 10; x++)
+    for(var x = 0; x < 9; x++)
     {
         var avgprice = unpack(rows, 'AveragePrice', regions[x]);
         var totalbags = unpack(rows, 'Total Bags', regions[x]);
         prices[x] = avgprice;
         bags[x] = totalbags;
-        if(x != 7) {
-            total += bags[x];
-        }
+        total += bags[x];
     }
 
-    var percentages = new Array(10);
-    for(var x = 0; x < 10; x++)
+    var percentages = new Array(9);
+    for(var x = 0; x < 9; x++)
     {
-        if(x == 7)
-        {
-            percentages[x] = 1.0;
-        }
-        else
-        {
-            percentages[x] = bags[x] / total;
-        }
+        percentages[x] = bags[x] / total;
     }
 
     var trace1 = {
         x: regions,
         y: prices,//price,
-        text: ['GreatLakes<br>percentage of total US: ' + percentages[0], 'Midsouth<br>percentage of total US: ' + percentages[1], 'Northeast<br>percentage of total US: ' + percentages[2], 'NorthernNewEngland<br>percentage of total US: ' + percentages[3], 'Plains<br>percentage of total US: ' + percentages[4], 'SouthCentral<br>percentage of total US: ' + percentages[5], 'Southeast<br>percentage of total US: ' + percentages[6], 'TotalUS<br>percentage of total US: ' + percentages[7], 'West<br>percentage of total US: ' + percentages[8], 'WestTexNewMexico<br>percentage of total US: ' + percentages[9]],
+        text: ['GreatLakes<br>percentage of total US: ' + percentages[0], 'Midsouth<br>percentage of total US: ' + percentages[1], 'Northeast<br>percentage of total US: ' + percentages[2], 'NorthernNewEngland<br>percentage of total US: ' + percentages[3], 'Plains<br>percentage of total US: ' + percentages[4], 'SouthCentral<br>percentage of total US: ' + percentages[5], 'Southeast<br>percentage of total US: ' + percentages[6], 'West<br>percentage of total US: ' + percentages[7], 'WestTexNewMexico<br>percentage of total US: ' + percentages[8]],
         mode: 'markers',
         marker: {
             size: bags,//x_axis,
