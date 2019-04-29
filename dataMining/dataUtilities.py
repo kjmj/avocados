5f8ce2c3baf77f1f647d102538ed0fb16c6f70fb
Ken Morton Jr <kmorton15@gmail.com>
2019-04-29 01:04:11 -0400

    stacked bar and maps done

--- dataMining/dataUtilities.py
+++ dataMining/dataUtilities.py
@@ -5,3 +5,14 @@ import numpy as np
 def normalizeData(data_array):
     return (data_array - np.min(data_array)) / (np.max(data_array) - np.min(data_array))
 
+# Replace any number of spaces with just a single space
+def refineSpaces(x):
+    return x.replace('\s+', ' ', regex=True)
+
+# Remove leading and trailing white space
+def removeExcessWhitespace(x):
+    return x.str.strip()
+
+# Convert the string to uppercase
+def toUpper(x):
+    return x.str.upper()
